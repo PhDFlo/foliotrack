bf813a4f20ba44845320e381b1cd7f99a5e34c22
Florian Granger <florian.granger43@gmail.com>
2025-07-13 21:09:02 +0200

    First version of the equilibrium (not working)

--- main.py
+++ main.py
@@ -10,20 +10,31 @@ def main():
     
     # Create a PortfolioETF instance
     portfolio = PortfolioETF()
-    portfolio.add_etf(etf1, portfolio_share=0.5, amount_invested=1000.0)
-    portfolio.add_etf(etf2, portfolio_share=0.3, amount_invested=600.0)
-    portfolio.add_etf(etf3, portfolio_share=0.2, amount_invested=400.0)
+    portfolio.add_etf(etf1, portfolio_share=0.5, amount_invested=2000.0)
+    portfolio.add_etf(etf2, portfolio_share=0.2, amount_invested=800.0)
+    portfolio.add_etf(etf3, portfolio_share=0.3, amount_invested=300.0)
     
     # Compute actual shares based on invested amounts
     actual_shares = portfolio.compute_actual_shares()
     
-    # Show for portfolio equilibrium
-    pmatrix = portfolio.generate_shares_matrix(etf1)
-    
-    print(pmatrix)
-    
     # Verify portfolio shares
     portfolio.verify_shares_sum()
+    
+    # Show for portfolio equilibrium
+    #pmatrix = portfolio.generate_shares_matrix(etf1)
+    
+    # Generate vector of shares
+    #invested_vector = portfolio.generate_invested_vector(etf1)
+    
+    # Solve for equilibrium
+    equilibrium = portfolio.solve_equilibrium(etf1)
+    
+    for elem in equilibrium:
+        #print(elem)
+        print(f"ETF: {elem['name']}")
+        print(f"   Share: {elem['portfolio_share']}, Final Share: {elem['final_share']}")
+        print(f"   Amount Invested: {elem['amount_invested']}€, Amount to Invest: {elem['amount_to_invest']}€")
+
 
 
 if __name__ == "__main__":
