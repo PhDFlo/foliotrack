8fcd6ba1a99df136dbd217686b25d80c3d1f340d
PhDFlo <florian.granger43@gmail.com>
2025-07-16 22:32:51 +0200

    Add folder refresh

--- app.py
+++ app.py
@@ -45,20 +45,33 @@ def optimize_portfolio(etf_data, new_investment):
     portfolio_data = pd.DataFrame(portfolio_data)
     return portfolio_data
 
+# Function to update the ETF table from a CSV file
 def update_table_from_file(inp):
     # Read initial data from CSV or create an empty DataFrame
     df = pd.read_csv(inp, delimiter=",")
     df = df.to_numpy()  # Convert DataFrame to numpy array for Gradio compatibility
     return df.tolist()
-   
+
+# Function to update file explorer
+def update_file_explorer():
+    return gr.FileExplorer(root_dir="/")
+def update_file_explorer_2():
+    return gr.FileExplorer(root_dir="./Inputs")
+
 with gr.Blocks() as demo:
     gr.Markdown("# ETF Portfolio Optimizer")
     
+    # File explorer to select CSV files
     inp = gr.FileExplorer(root_dir="./Inputs", 
                 value="investment.csv",
                 label="CSV Files available", 
                 file_count='single')
     
+    # Button to refresh file explorer
+    btn_refresh = gr.Button("Refresh available files")
+    btn_refresh.click(update_file_explorer, outputs=inp).then(update_file_explorer_2, outputs=inp)
+    
+    # ETF table for user input
     etf_table = gr.Dataframe(
         headers=["Name", "Ticker", "Currency", "Price", "Fees", "Target Share", "Amount Invested"],
         datatype=["str", "str", "str", "number", "number", "number", "number"],
@@ -69,11 +82,14 @@ with gr.Blocks() as demo:
         column_widths=['10%', '5%', '5%', '5%', '5%', '5%', '5%']  # Adjusted widths for better visibility
     )
 
+    # Button to fill table from CSV file
     btn_refresh = gr.Button("Fill Table from CSV (optional)")
     btn_refresh.click(update_table_from_file, inputs=inp, outputs=etf_table)
      
+    # Input for new investment amount
     new_investment = gr.Number(label="New Investment Amount", value=500.0)
 
+    # Dataframe to display equilibrium results
     equilibrium_table = gr.Dataframe(
         headers=["Name", "Price", "Target Share", "Amount Invested", "Actual Share", "Number to buy", "Final Share"],
         datatype=["str", "number", "number", "number", "number", "number", "number"],
@@ -81,6 +97,7 @@ with gr.Blocks() as demo:
         visible=True,
     )   
 
+    # Button to run optimization
     run_btn = gr.Button("Optimize Portfolio")
     run_btn.click(optimize_portfolio, inputs=[etf_table, new_investment], outputs=equilibrium_table)
 
