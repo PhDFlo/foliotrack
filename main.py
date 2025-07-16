62b1d4f36caca5653e4314603848ed655ef0c291
PhDFlo <florian.granger43@gmail.com>
2025-07-16 18:44:57 +0200

    Change problem to MIQP

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from DCA.ETF import ETF
 from DCA.Portfolio import PortfolioETF
 import numpy as np
-import matplotlib.pyplot as plt
 
 def main():
     
@@ -18,29 +17,7 @@ def main():
     
     # Solve for equilibrium
     #equilibrium = portfolio.solve_equilibrium(etf3, 0.)
-    equilibrium = portfolio.solve_equilibrium(etf1, 20.)
-    
-    for elem in equilibrium:
-        print(f"ETF: {elem['etf'].name}")
-        print(f"   Target share: {elem['portfolio_share']},  Actual share: {elem['actual_share']}, Final Share: {elem['final_share']}")
-        print(f"   Amount Invested: {elem['amount_invested']}€, Amount to Invest: {elem['amount_to_invest']}€")
-
-    print(f"Total to invest: {portfolio.total_to_invest}€")
-    
-    # # Loop over the invested moment
-    # Select_ETF = etf1
-    # Tot_invest_list = []
-    # Invest_val = np.linspace(0., 1000., 100)
-    
-    # for val in Invest_val:
-    #     equilibrium = portfolio.solve_equilibrium(Select_ETF, val)
-    #     print("")
-    #     Tot_invest_list.append(portfolio.total_to_invest)
-        
-    # # Plot evolution
-    # plt.figure()
-    # plt.plot(Invest_val, Tot_invest_list)
-    # plt.savefig("test.png")
+    equilibrium = portfolio.solve_equilibrium(1000.0)
         
 
 if __name__ == "__main__":
