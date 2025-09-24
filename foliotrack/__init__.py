b0a36e28be349e2b327375c5efb481128c4e54fd
PhDFlo <florian.granger43@gmail.com>
2025-09-24 20:30:44 +0200

    Add all methods

--- foliotrack/__init__.py
+++ foliotrack/__init__.py
@@ -5,4 +5,4 @@ from .Equilibrate import Equilibrate
 from .Portfolio import Portfolio
 from .Security import Security
 
-__all__ = ["Currency", "Security", "Portfolio", "Equilibrate"]
+__all__ = ["Currency", "Security", "Portfolio", "Equilibrate", "get_symbol", "get_currency_name", "get_currency_code_from_symbol", "get_rate_between"]
